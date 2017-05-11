889a0d93beb739147fb6c0c575284e63ee311f31
Domgrar <icaruspsy@gmail.com>
2017-05-10 20:44:40 -0400

    First change to github java file

--- Githubtrial.java
+++ Githubtrial.java
@@ -16,6 +16,7 @@ public class Githubtrial {
      */
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("This is the first change");
     }
     
 }
